92db4345480b9fbad54d46b0257e6a847421c094
Neeraj Sharma <neeraj@archive.org>
2020-02-13 07:51:15 +0000

    null

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { LightningElement, api, buildCustomElementConstructor } from "lwc";
 
 export default class Foo extends LightningElement {
-  @api name = "World123";
+  @api name = "World";
 }
 
 customElements.define("custom-element", buildCustomElementConstructor(Foo));
